9b93c4af00a4863808925e06f792020ad7302a29
rchaser53 <tayoshizawa29@gmail.com>
2019-02-01 22:44:41 +0900

    try to express minus in peano

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ const createPeano = (num, s = {}) => {
 }
 
 const three = createPeano(3)
+const two = createPeano(2)
 const one = createPeano(1)
 
 const incrementalPeano = (p1) => {
   return { s: p1 }
+}
+
+const decrementalPeano = (p1) => {
+  return p1.s
 }
 
 const addPeano = (p1, p2) => {
@@ -22,19 +27,31 @@ const addPeano = (p1, p2) => {
   }
 }
 
+const minusPeano = (p1, p2) => {
+  if (p2.s == null) {
+    return p1;
+  } else {
+    p2 = p2.s
+    p1 = decrementalPeano(p1)
+    return minusPeano(p1, p2)
+  }
+}
+
 console.log(
-  // incrementalPeano(one),
   JSON.stringify(
     three
   ),
   JSON.stringify(
     addPeano(one, three),
-  ) 
+  ),
+  JSON.stringify(
+    minusPeano(three, two),
+  )
 )
 
-// console.log(
-//   createPeano(0),
-//   createPeano(1),
-//   createPeano(2),
-//   JSON.stringify(createPeano(10)),
-// )
+
+/**
+ * z        + n   =  n        P-Zero
+ * S(z)     + n   =  S(n)     P-Succ
+ * S(S(z))  + n   =  S(S(n))  P-Succ
+ */
